e98a5ddec2f6356e5cfa97bcfe2c33c4de9a67ae
luisgr94 <luisdaniel.guerrero94@gmail.com>
2019-12-16 23:19:14 -0600

    spotiapp-2

---
+++ 4. spotiapp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Importar Rutas
+
+import { ROUTES } from "./app.routes";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot( ROUTES, { useHash: true } )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
